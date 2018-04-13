8aef48f7b874f6fd29e51abbb1b506bfed2536f4
Nicco Kunzmann <niccokunzmann@googlemail.com>
2018-04-13 08:09:00 +0200

    Create install.sh

---
+++ software/install.sh
@@ -0,0 +1,7 @@
+# Install Sane
+# from https://www.cyberciti.biz/faq/linux-scan-image-commands/
+sudo apt-get install sane sane-utils
+
+# Install simple scan
+# from https://www.linuxquestions.org/questions/linux-hardware-18/scanning-using-scanner-canon-lide-120-canon-with-sane-4175609129/
+sudo apt-get install simple-scan
