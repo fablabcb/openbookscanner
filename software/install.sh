5aa53083b478ef891f8e56d1997b3ce218155b05
Nicco Kunzmann <niccokunzmann@googlemail.com>
2018-04-13 10:33:37 +0200

    add imagemagick

--- software/install.sh
+++ software/install.sh
@@ -5,3 +5,11 @@ sudo apt-get install sane sane-utils
 # Install simple scan
 # from https://www.linuxquestions.org/questions/linux-hardware-18/scanning-using-scanner-canon-lide-120-canon-with-sane-4175609129/
 sudo apt-get install simple-scan
+
+# Install imagemagick to convert images
+# from http://dev-random.net/convert-multiple-jpg-or-png-to-pdf-in-linux/
+# this provides the "convert" command
+sudo apt-get install ghostscript imagemagick
+
+echo "In order to scan, please reboot"
+echo "sudo reboot"
