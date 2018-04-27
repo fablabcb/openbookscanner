2dc431fcef476045e8e745419dbba397bfc52a1a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-04-27 20:56:14 +0200

    add callback to strategy

--- software/openbookscanner/update_strategy.py
+++ software/openbookscanner/update_strategy.py
@@ -1,29 +1,42 @@
 from parse_rest.connection import ParseBatcher
 import collections
 
+class NoCallback:
+
+    def __call__(self):
+        """Nothing happens."""
+    
+    def __repr__(self):
+        """String represenatation."""
+        return self.__class__.__name__ + "()"
+
 
 class OnChangeStrategy:
     """When an object changes, it is saved asap."""
     
-    def save(self, obj):
+    def save(self, obj, callback=NoCallback()):
         """Save the object."""
         obj.save()
+        callback()
     
-    def delete(self, obj):
+    def delete(self, obj, callback=NoCallback()):
         """Delete the object."""
         obj.delete()
+        callback()
         
-    def addToArray(self, obj, array_name, objects):
+    def addToArray(self, obj, array_name, objects, callback=NoCallback()):
         """Add objects to a named array."""
         # from https://stackoverflow.com/a/1952481
         assert isinstance(objects, collections.Iterable) and not isinstance(objects, str), "Please pass a list of objects."
         obj.addToArray(array_name, objects)
+        callback()
     
-    def removeFromArray(self, obj, array_name, objects):
+    def removeFromArray(self, obj, array_name, objects, callback=NoCallback()):
         """Remove objects from a named array."""
         # from https://stackoverflow.com/a/1952481
         assert isinstance(objects, collections.Iterable) and not isinstance(objects, str), "Please pass a list of objects."
         obj.removeFromArray(array_name, objects)
+        callback()
 
     def __repr__(self):
         """String representation."""
@@ -44,13 +57,13 @@ class BatchStrategy(OnChangeStrategy):
         super().__init__()
         self._batch = []
     
-    def save(self, obj):
+    def save(self, obj, callback=NoCallback()):
         """Save the object."""
-        self._batch.append(obj.save)
+        self._batch.append((obj.save, callback))
 
-    def delete(self, obj):
+    def delete(self, obj, callback=NoCallback()):
         """Delete the object."""
-        self._batch.append(obj.delete)
+        self._batch.append((obj.delete, callback))
     
     # TODO: implement batch requests for object add and remove.
 #    def addToArray(self, obj, key, value): 
@@ -64,9 +77,11 @@ class BatchStrategy(OnChangeStrategy):
             while self._batch:
                 # can send 50 operations
                 # http://docs.parseplatform.org/rest/guide/#batch-operations
-                batch = self._batch[:50]
+                batch, callbacks = zip(*self._batch[:50])
                 self._batch = self._batch[50:]
                 batcher.batch(batch)
+                for callback in callbacks:
+                    callback()
      
     new_batcher = ParseBatcher
 
