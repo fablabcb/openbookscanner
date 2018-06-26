53303924a470c4260868509a567e4da6d36e4f10
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-06-26 21:05:11 +0200

    add posting method

--- software/OpenBookScannerApp/app/src/main/java/com/github/fablabcb/openbookscannerapp/MainActivity.java
+++ software/OpenBookScannerApp/app/src/main/java/com/github/fablabcb/openbookscannerapp/MainActivity.java
@@ -6,14 +6,28 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean hasCameraFeature;
     private boolean cameraIsDisabled;
     private Camera camera;
+    private EditText addressIP;
+    private EditText addressPort;
+    private TextView serverStatus;
+    private UUID app_id;
 
 
 
@@ -36,7 +54,12 @@ public class MainActivity extends AppCompatActivity {
         statusText = (TextView) findViewById(R.id.statusText);
         button = (Button) findViewById(R.id.takePictureButton);
         PackageManager pm = getApplicationContext().getPackageManager();
+        addressIP = (EditText) findViewById(R.id.addressIP);
+        addressPort = (EditText) findViewById(R.id.addressPort);
+        serverStatus = (TextView) findViewById(R.id.serverStatus);
         hasCameraFeature =  pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+        // from https://stackoverflow.com/questions/2982748/create-a-guid-in-java#2982751
+        app_id = java.util.UUID.randomUUID();;
     }
 
     private int findFrontFacingCamera() {
@@ -84,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
         }
+        notifyServer();
     }
 
     protected void setCameraParameters() {
@@ -144,4 +168,111 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
         }
     }
+
+    public int getPort() {
+        // from https://stackoverflow.com/a/25804635/1320237
+        return Integer.parseInt( addressPort.getText().toString() );
+    }
+
+    public String getHostname() {
+        return addressPort.getText().toString();
+    }
+
+    public URL getServerUrl() throws MalformedURLException {
+        return new URL("http://" + getHostname() + ":" + getPort() + "/scanner");
+    }
+
+    protected void notifyServer() {
+        URL url;
+        try {
+            url = getServerUrl();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            setServerStatus(R.string.server_status_malformed_url);
+            return;
+        }
+        // see https://www.wikihow.com/Execute-HTTP-POST-Requests-in-Android
+        HttpURLConnection client;
+        try {
+            client = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            setServerStatus(R.string.server_status_connection_error);
+            e.printStackTrace();
+            return;
+        }
+        try {
+            client.setRequestMethod("POST");
+        } catch (ProtocolException e) {
+            setServerStatus(R.string.server_status_protocol_exception);
+            e.printStackTrace();
+            return;
+        }
+        // from https://stackoverflow.com/a/34555324/1320237
+        client.addRequestProperty("Accept", "application/json");
+        client.addRequestProperty("Content-Type", "application/json");
+        client.setDoOutput(true);
+        // write data to the server
+        DataOutputStream os;
+        // from https://stackoverflow.com/q/42767249/1320237
+        JSONObject notification = new JSONObject();
+        try {
+            notification.put("type", "scanner");
+            notification.put("name", getDeviceName());
+            notification.put("id", app_id.toString());
+        } catch (JSONException e) {
+            setServerStatus(R.string.server_status_create_notification_exception);
+            e.printStackTrace();
+            return;
+        }
+        try {
+            os = new DataOutputStream(client.getOutputStream());
+            os.writeBytes(URLEncoder.encode(notification.toString(), "UTF-8"));
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            setServerStatus(R.string.server_status_send_notification_exception);
+            e.printStackTrace();
+            return;
+        }
+        Log.d(DEBUG_TAG, "POST request sent.");
+        try {
+            Log.i("STATUS", String.valueOf(client.getResponseCode()));
+            Log.i("MSG" , client.getResponseMessage());
+        } catch (IOException e) {
+            setServerStatus(R.string.server_status_get_server_response);
+            e.printStackTrace();
+            return;
+        }
+
+    }
+
+    private void setServerStatus(int resourceId) {
+        serverStatus.setText(resourceId);
+    }
+
+    public String getDeviceName() {
+        // from https://stackoverflow.com/a/12707479/1320237
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.toLowerCase().startsWith(manufacturer.toLowerCase())) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+
+    private String capitalize(String s) {
+        // form https://stackoverflow.com/a/12707479/1320237
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
+        }
+    }
+
 }
